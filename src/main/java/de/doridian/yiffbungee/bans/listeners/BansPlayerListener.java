9cd12d6b4702d461bb9ed6bd41ba011d13ef7330
Doridian Draconia <mriq91@gmail.com>
2014-05-08 20:54:39 +0200

    Use LoginEvent

--- src/main/java/de/doridian/yiffbungee/bans/listeners/BansPlayerListener.java
+++ src/main/java/de/doridian/yiffbungee/bans/listeners/BansPlayerListener.java
@@ -3,14 +3,14 @@ package de.doridian.yiffbungee.bans.listeners;
 import de.doridian.yiffbungee.bans.Ban;
 import de.doridian.yiffbungee.bans.BanResolver;
 import de.doridian.yiffbungee.main.YiffBungeeListener;
-import net.md_5.bungee.api.event.PreLoginEvent;
+import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.event.EventHandler;
 
 import java.util.UUID;
 
 public class BansPlayerListener extends YiffBungeeListener {
 	@EventHandler
-	public void onPlayerPreLogin(PreLoginEvent event) {
+	public void onPlayerPreLogin(LoginEvent event) {
 		String name = event.getConnection().getName();
 		UUID uuid = event.getConnection().getUniqueId();
 
