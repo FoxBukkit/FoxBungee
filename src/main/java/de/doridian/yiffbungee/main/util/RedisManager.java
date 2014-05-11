2229992d3ea199766787bf785b9d805d17aaf4f1
Doridian Draconia <mriq91@gmail.com>
2014-05-11 16:09:29 +0200

    Move stuff to RedisManager

--- src/main/java/de/doridian/yiffbungee/main/util/RedisManager.java
+++ src/main/java/de/doridian/yiffbungee/main/util/RedisManager.java
@@ -4,22 +4,21 @@ import de.doridian.yiffbungee.config.Configuration;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisPubSub;
 
 import java.util.*;
 
 public class RedisManager {
-	public static JedisPool readJedisPool;
-	public static JedisPool[] writeJedisPools;
+    private static JedisPool jedisPool;
 
-	private static String REDIS_PASSWORD;
-	private static int REDIS_DB;
+    private static String REDIS_PASSWORD;
+    private static int REDIS_DB;
 
-	public static void initialize() {
-		REDIS_PASSWORD = Configuration.getValue("redis-pw", "");
-		REDIS_DB = Integer.parseInt(Configuration.getValue("redis-db", "1"));
-		readJedisPool = createPool(Configuration.getValue("redis-host", ""));
-		writeJedisPools = new JedisPool[] { readJedisPool };
-	}
+    public static void initialize() {
+        REDIS_PASSWORD = Configuration.getValue("redis-pw", "");
+        REDIS_DB = Integer.parseInt(Configuration.getValue("redis-db", "1"));
+        jedisPool = createPool(Configuration.getValue("redis-host", ""));
+    }
 
     private static JedisPool createPool(String host) {
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
@@ -32,130 +31,258 @@ public class RedisManager {
         return new JedisPool(jedisPoolConfig, host, 6379, 1000, REDIS_PASSWORD, REDIS_DB);
     }
 
-	public static class RedisMap implements Map<String, String> {
-		private final String name;
-		public RedisMap(String name) {
-			this.name = name;
-		}
+    public static long hlen(String key) {
+        Jedis jedis = null;
+        while(true) {
+            try {
+                jedis = jedisPool.getResource();
 
-		@Override
-		public int size() {
-			Jedis jedis = readJedisPool.getResource();
+                long ret;
+                if (!jedis.exists(key))
+                    ret = 0L;
+                else
+                    ret = jedis.hlen(key);
 
-			int ret;
-			if(!jedis.exists(name))
-				ret = 0;
-			else
-				ret = (int)(long)jedis.hlen(name);
+                jedisPool.returnResource(jedis);
 
-			readJedisPool.returnResource(jedis);
+                return ret;
+            } catch (Exception e) {
+                e.printStackTrace();
+                if(jedis != null)
+                    jedisPool.returnBrokenResource(jedis);
+            }
+        }
+    }
 
-			return ret;
-		}
+    public static boolean hexists(String key, String index) {
+        Jedis jedis = null;
+        while(true) {
+            try {
+                jedis = jedisPool.getResource();
+                boolean exists = jedis.hexists(key, index);
+                jedisPool.returnResource(jedis);
+                return exists;
+            } catch (Exception e) {
+                e.printStackTrace();
+                if(jedis != null)
+                    jedisPool.returnBrokenResource(jedis);
+            }
+        }
+    }
 
-		@Override
-		public boolean isEmpty() {
-			return (size() <= 0);
-		}
+    public static String hget(String key, String index) {
+        Jedis jedis = null;
+        while(true) {
+            try {
+                jedis = jedisPool.getResource();
+                String value = jedis.hget(key, index);
+                jedisPool.returnResource(jedis);
+                return value;
+            } catch (Exception e) {
+                e.printStackTrace();
+                if(jedis != null)
+                    jedisPool.returnBrokenResource(jedis);
+            }
+        }
+    }
 
-		@Override
-		public boolean containsKey(Object key) {
-			Jedis jedis = readJedisPool.getResource();
-			boolean exists = jedis.hexists(name, key.toString());
-			readJedisPool.returnResource(jedis);
-			return exists;
-		}
+    public static void hset(String key, String index, String value) {
+        Jedis jedis = null;
+        while(true) {
+            try {
+                jedis = jedisPool.getResource();
+                jedis.hset(key, index, value);
+                jedisPool.returnResource(jedis);
+                return;
+            } catch (Exception e) {
+                e.printStackTrace();
+                if(jedis != null)
+                    jedisPool.returnBrokenResource(jedis);
+            }
+        }
+    }
 
-		@Override
-		public boolean containsValue(Object value) {
-			return values().contains(value.toString());
-		}
+    public static void hdel(String key, String index) {
+        Jedis jedis = null;
+        while(true) {
+            try {
+                jedis = jedisPool.getResource();
+                jedis.hdel(key, index);
+                jedisPool.returnResource(jedis);
+                return;
+            } catch (Exception e) {
+                e.printStackTrace();
+                if(jedis != null)
+                    jedisPool.returnBrokenResource(jedis);
+            }
+        }
+    }
 
-		@Override
-		public String get(Object key) {
-			Jedis jedis = readJedisPool.getResource();
-			String ret = jedis.hget(name, key.toString());
-			readJedisPool.returnResource(jedis);
-			return ret;
-		}
+    public static Set<String> hkeys(String key) {
+        Jedis jedis = null;
+        while(true) {
+            try {
+                jedis = jedisPool.getResource();
+                Set<String> keys = jedis.hkeys(key);
+                jedisPool.returnResource(jedis);
+                return keys;
+            } catch (Exception e) {
+                e.printStackTrace();
+                if(jedis != null)
+                    jedisPool.returnBrokenResource(jedis);
+            }
+        }
+    }
 
-		@Override
-		public Set<String> keySet() {
-			Jedis jedis = readJedisPool.getResource();
-			Set<String> keys = jedis.hkeys(name);
-			readJedisPool.returnResource(jedis);
-			if(keys == null)
-				return new HashSet<String>();
-			return keys;
-		}
+    public static List<String> hvals(String key) {
+        Jedis jedis = null;
+        while(true) {
+            try {
+                jedis = jedisPool.getResource();
+                List<String> values = jedis.hvals(key);
+                jedisPool.returnResource(jedis);
+                return values;
+            } catch (Exception e) {
+                e.printStackTrace();
+                if(jedis != null)
+                    jedisPool.returnBrokenResource(jedis);
+            }
+        }
+    }
 
-		@Override
-		public Collection<String> values() {
-			Jedis jedis = readJedisPool.getResource();
-			Collection<String> values = jedis.hvals(name);
-			readJedisPool.returnResource(jedis);
-			if(values == null)
-				return Collections.emptyList();
-			return values;
-		}
+    public static Map<String, String> hgetAll(String key) {
+        Jedis jedis = null;
+        while(true) {
+            try {
+                jedis = jedisPool.getResource();
+                Map<String, String> values = jedis.hgetAll(key);
+                jedisPool.returnResource(jedis);
+                return values;
+            } catch (Exception e) {
+                e.printStackTrace();
+                if(jedis != null)
+                    jedisPool.returnBrokenResource(jedis);
+            }
+        }
+    }
 
-		@Override
-		public Set<Entry<String, String>> entrySet() {
-			Jedis jedis = readJedisPool.getResource();
-			Map<String, String> entryMap = jedis.hgetAll(name);
-			readJedisPool.returnResource(jedis);
-			if(entryMap == null)
-				return Collections.emptySet();
-			return entryMap.entrySet();
-		}
+    public static void subscribe(String key, JedisPubSub listener) throws Exception {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            jedis.subscribe(listener, key);
+            jedisPool.returnResource(jedis);
+        } catch (Exception e) {
+            if(jedis != null)
+                jedisPool.returnBrokenResource(jedis);
+            throw e;
+        }
+    }
 
-		@Override
-		public String put(String key, String value) {
-			String old = get(key);
-			for(JedisPool writeJedisPool : writeJedisPools) {
-				Jedis jedis = writeJedisPool.getResource();
-				jedis.hset(name, key, value);
-				writeJedisPool.returnResource(jedis);
-			}
-			return old;
-		}
+    public static void publish(String key, String value) {
+        Jedis jedis = null;
+        while(true) {
+            try {
+                jedis = jedisPool.getResource();
+                jedis.publish(key, value);
+                jedisPool.returnResource(jedis);
+            } catch (Exception e) {
+                e.printStackTrace();
+                if(jedis != null)
+                    jedisPool.returnBrokenResource(jedis);
+            }
+        }
+    }
 
-		@Override
-		public String remove(Object key) {
-			String old = get(key);
-			String keyS = key.toString();
-			for(JedisPool writeJedisPool : writeJedisPools) {
-				Jedis jedis = writeJedisPool.getResource();
-				jedis.hdel(name, keyS);
-				writeJedisPool.returnResource(jedis);
-			}
-			return old;
-		}
+    public static class RedisMap implements Map<String, String> {
+        private final String name;
+        public RedisMap(String name) {
+            this.name = name;
+        }
 
-		@Override
-		public void putAll(Map<? extends String, ? extends String> m) {
-			for(JedisPool writeJedisPool : writeJedisPools) {
-				Jedis jedis = writeJedisPool.getResource();
-				for(Entry<? extends String, ? extends String> e : m.entrySet()) {
-					jedis.hset(name, e.getKey(),  e.getValue());
-				}
-				writeJedisPool.returnResource(jedis);
-			}
-		}
+        @Override
+        public int size() {
+            return (int)hlen(name);
+        }
 
-		@Override
-		public void clear() {
-			throw new RuntimeException();
-		}
-	}
+        @Override
+        public boolean isEmpty() {
+            return (size() <= 0);
+        }
 
-	public static Map<String,String> createRedisMap(String name) {
-		return new RedisMap(name);
-	}
-	public static Map<String,String> createCachedRedisMap(String name) {
-		return createCachedRedisMap(name, 10000L);
-	}
-	public static Map<String,String> createCachedRedisMap(String name, long expiry) {
-		return new CacheMap(expiry, name, new RedisMap(name));
-	}
+        @Override
+        public boolean containsKey(Object key) {
+            return hexists(name, key.toString());
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return values().contains(value.toString());
+        }
+
+        @Override
+        public String get(Object key) {
+            return hget(name, key.toString());
+        }
+
+        @Override
+        public Set<String> keySet() {
+            Set<String> keys = hkeys(name);
+            if(keys == null)
+                return new HashSet<>();
+            return keys;
+        }
+
+        @Override
+        public Collection<String> values() {
+            Collection<String> values = hvals(name);
+            if(values == null)
+                return Collections.emptyList();
+            return values;
+        }
+
+        @Override
+        public Set<Entry<String, String>> entrySet() {
+            Map<String, String> entryMap = hgetAll(name);
+            if(entryMap == null)
+                return Collections.emptySet();
+            return entryMap.entrySet();
+        }
+
+        @Override
+        public String put(String key, String value) {
+            String old = get(key);
+            hset(name, key, value);
+            return old;
+        }
+
+        @Override
+        public String remove(Object key) {
+            String old = get(key);
+            hdel(name, key.toString());
+            return old;
+        }
+
+        @Override
+        public void putAll(Map<? extends String, ? extends String> m) {
+            for(Entry<? extends String, ? extends String> e : m.entrySet()) {
+                put(e.getKey(), e.getValue());
+            }
+        }
+
+        @Override
+        public void clear() {
+            throw new RuntimeException();
+        }
+    }
+
+    public static Map<String,String> createRedisMap(String name) {
+        return new RedisMap(name);
+    }
+    public static Map<String,String> createCachedRedisMap(String name) {
+        return createCachedRedisMap(name, 10000L);
+    }
+    public static Map<String,String> createCachedRedisMap(String name, long expiry) {
+        return new CacheMap(expiry, name, new RedisMap(name));
+    }
 }
