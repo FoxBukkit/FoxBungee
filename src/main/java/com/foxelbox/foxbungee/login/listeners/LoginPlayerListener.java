6113052c0e7082ca91d083a092194a529b981acc
Mark Dietzer <mriq91@gmail.com>
2015-07-23 13:22:59 +0200

    We were too late

--- src/main/java/com/foxelbox/foxbungee/login/listeners/LoginPlayerListener.java
+++ src/main/java/com/foxelbox/foxbungee/login/listeners/LoginPlayerListener.java
@@ -19,14 +19,14 @@ package com.foxelbox.foxbungee.login.listeners;
 import com.foxelbox.foxbungee.main.FoxBungee;
 import com.foxelbox.foxbungee.main.FoxBungeeListener;
 import net.md_5.bungee.api.connection.PendingConnection;
-import net.md_5.bungee.api.event.LoginEvent;
+import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.event.EventHandler;
 
 import java.util.UUID;
 
 public class LoginPlayerListener extends FoxBungeeListener {
     @EventHandler
-    public void onPlayerPreLogin(LoginEvent event) {
+    public void onPlayerPreLogin(PreLoginEvent event) {
         final PendingConnection pendingConnection = event.getConnection();
         final String name = pendingConnection.getName();
         final String vHost = pendingConnection.getVirtualHost().getHostName();
